a07de28a75f4ec8786d41c5bf0d95845f4719e5c
AdnanA2 <97619642+AdnanA2@users.noreply.github.com>
2022-03-22 16:49:39 -0400

    SomeWhere

--- backend-api/src/main/java/org/wcci/apimastery/Populator.java
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -30,7 +30,7 @@ public class Populator implements CommandLineRunner {
 
 
 
-        Album album2 = new Album("Thriller","Michael Jackson", "","Atlantic Records",  5);
+        Album album2 = new Album("Thriller","Michael Jackson", "images/MJThriller.png","Atlantic Records",  5);
         album2.addComment(new Comment("Adnan's Life is this album"));
         albumRepo.save(album2);
         Song BillieJean = new Song("BillieJean", "4:15",2, album2);
@@ -38,7 +38,7 @@ public class Populator implements CommandLineRunner {
         songRepo.save(BillieJean);
 
 
-        Album album3 = new Album("Starboy","The Weeknd","", "Republican Records", 5);
+        Album album3 = new Album("Starboy","The Weeknd","images/Starboy.jpg", "Republican Records", 5);
         album3.addComment(new Comment("Ryan's Life is this album"));
         albumRepo.save(album3);
         Song Starboy = new Song("Starboy", "3:15", 1,album3);
@@ -46,7 +46,7 @@ public class Populator implements CommandLineRunner {
         songRepo.save(Starboy);
 
 
-        Album album4 = new Album("My Prerogative", "Bon Jovi","", "Zomba Records",5);
+        Album album4 = new Album("My Prerogative", "Bon Jovi","images/Brittany.jpg", "Zomba Records",5);
         album4.addComment(new Comment("I meant to say that to make him mad's Life is this album"));
         albumRepo.save(album4);
         Song doSomethin = new Song("Do Somethin","3:24",2,album4);
@@ -54,7 +54,7 @@ public class Populator implements CommandLineRunner {
         songRepo.save(doSomethin);
 
 
-        Album album5 = new Album("Out of the Blue", "ELO","", "Epic Records",5);
+        Album album5 = new Album("Out of the Blue", "ELO","images/MrBlueSky.jpg", "Epic Records",5);
         album5.addComment(new Comment("David's Life is this album"));
         albumRepo.save(album5);
         Song mrBlueSky = new Song("Mr. Blue Sky", "5:03", 5,album5);
@@ -62,7 +62,7 @@ public class Populator implements CommandLineRunner {
         songRepo.save(mrBlueSky);
 
 
-        Album album6 = new Album("A Night at the Opera", "Queen","", "Hollywood Records", 5);
+        Album album6 = new Album("A Night at the Opera", "Queen","images/Queen.jpg", "Hollywood Records", 5);
         album6.addComment(new Comment("Ricky's Life is this album"));
         albumRepo.save(album6);
         Song bohemianRhapsody = new Song("Bohemian Rhapsody", "5:54", 5, album6);
