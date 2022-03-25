cf2c190f0dc58496f589324d500d9281583c6122
AdnanA2 <97619642+AdnanA2@users.noreply.github.com>
2022-03-25 14:12:44 -0400

    Maybe it is it

--- backend-api/src/main/java/org/wcci/apimastery/Populator.java
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -20,36 +20,36 @@ public class Populator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Album album1 = new Album("4","Foreigner", "images/Foreigner4.jpg","2002 Atlantic Recording Corp.",5);
-        album1.addComment(new Comment("Dallas favorite song",2));
+        album1.addComment(new Comment("Dallas favorite song.",2));
         albumRepo.save(album1);
         Song JukeBoxHero = new Song("Juke Box Hero", "4:19", 5, album1, "https://open.spotify.com/embed/track/00qOE7OjRl0BpYiCiweZB2?utm_source=generator" );
-        JukeBoxHero.addComment(new Comment("Lastly, Ben the wife smuggler made this object caps to, RIP to the crew",3));
+        JukeBoxHero.addComment(new Comment("Dallas is the stuff to this song",3));
         songRepo.save(JukeBoxHero);
 
 
 
 
         Album album2 = new Album("Thriller","Michael Jackson", "images/MJThriller.png"," 1982, 2001, 2008 MJJ Productions Inc.",  5);
-        album2.addComment(new Comment("Adnan's Life is this album",1));
+        album2.addComment(new Comment("Ben's Life is this album",1));
         albumRepo.save(album2);
         Song BillieJean = new Song("BillieJean", "4:15",2, album2,"https://open.spotify.com/embed/track/3S2R0EVwBSAVMd5UMgKTL0?utm_source=generator");
-        BillieJean.addComment(new Comment("Poor Brian made this object capital, RIP his java chances",5));
+        BillieJean.addComment(new Comment("Ben is the stuff to this song.",5));
         songRepo.save(BillieJean);
 
 
         Album album3 = new Album("Starboy","The Weeknd","images/Starboy.jpg", " 2016 The Weeknd XO, Inc., Manufactured and Marketed by Republic Records, a Division of UMG Recordings, Inc.", 5);
-        album3.addComment(new Comment("Ryan's Life is this album",1));
+        album3.addComment(new Comment("Adnan's Life is this album",1));
         albumRepo.save(album3);
         Song Starboy = new Song("Starboy", "3:15", 1,album3,"https://open.spotify.com/embed/track/7MXVkk9YMctZqd1Srtv4MB?utm_source=generator");
-        Starboy.addComment(new Comment("This song is an object and shouldnt be capital, but Adnan is on the team.",4));
+        Starboy.addComment(new Comment("Adnan is the stuff to this song.",4));
         songRepo.save(Starboy);
 
 
         Album album4 = new Album("My Prerogative", "Britney Spears","images/Brittany.jpg", "This compilation (P) 2004 Zomba Recording LLC",5);
-        album4.addComment(new Comment("comment",4));
+        album4.addComment(new Comment("Brian's life is this comment",4));
         albumRepo.save(album4);
         Song doSomethin = new Song("Do Somethin","3:24",2,album4,"https://open.spotify.com/embed/track/01ACsk2knUnsfWeZp63nqa?utm_source=generator");
-        doSomethin.addComment(new Comment("Never really Heard it!",3));
+        doSomethin.addComment(new Comment("Brian is a Britney fan!",3));
         songRepo.save(doSomethin);
 
 
